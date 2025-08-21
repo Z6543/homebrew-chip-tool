fb5087e8417fd9d9bd1915d4ca31de80d8e0dbde
zh4ck <zoltan1.balazs@gmail.com>
2025-08-21 07:54:19 +0000

    Add build workflow and Homebrew formula for chip-tool

---
+++ Formula/chip-tool.rb
@@ -0,0 +1,20 @@
+class ChipTool < Formula
+  desc "Matter controller CLI tool from connectedhomeip"
+  homepage "https://github.com/project-chip/connectedhomeip"
+  url "https://github.com/project-chip/connectedhomeip.git", :using => :git, :tag => "v1.4.2.0"
+  version "v1.4.2.0"
+  depends_on "python@3.11"
+  depends_on "ninja"
+  depends_on "gn"
+  depends_on "llvm"
+
+  def install
+    system "scripts/activate.sh"
+    system "./scripts/examples/gn_build_example.sh", "examples/chip-tool", "out/chip-tool"
+    bin.install "out/chip-tool/chip-tool"
+  end
+
+  test do
+    system "#{bin}/chip-tool", "pairing", "onnetwork", "1", "20202021"
+  end
+end
