147abab26c34b48439b409c7d58300305ccbfc38
Z6543 <root@localhost>
2025-08-21 12:31:12 +0200

    Update chip-tool formula to use specific release URL and version

--- Formula/chip-tool.rb
+++ Formula/chip-tool.rb
@@ -2,8 +2,8 @@
 class ChipTool < Formula
   desc "Matter controller CLI tool from connectedhomeip"
   homepage "https://github.com/project-chip/connectedhomeip"
-  url "https://github.com/Z6543/homebrew-chip-tool/releases/download/main/chip-tool"
-  version "main"
+  url "https://github.com/Z6543/homebrew-chip-tool/releases/tag/v1.4.2.0"
+  version "v1.4.2.0"
   sha256 "a2415a02ecc0fea78a32f701c25cecfb313c06defd0a693e26a3afa57420721a" # Replace with actual sha256 if available
 
   def install
