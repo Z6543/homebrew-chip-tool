314a39f254cb5ef118edb0b463e8f7662785530e
Z6543 <root@localhost>
2025-08-21 12:26:16 +0200

    Update chip-tool formula to use release URL and simplify installation steps

--- Formula/chip-tool.rb
+++ Formula/chip-tool.rb
@@ -1,19 +1,16 @@
+
 class ChipTool < Formula
   desc "Matter controller CLI tool from connectedhomeip"
   homepage "https://github.com/project-chip/connectedhomeip"
-  url "https://github.com/project-chip/connectedhomeip.git", :using => :git, :tag => "v1.4.2.0"
-  version "v1.4.2.0"
-  depends_on "python@3.11"
-  depends_on "ninja"
-  depends_on "llvm"
+  url "https://github.com/Z6543/homebrew-chip-tool/releases/download/main/chip-tool"
+  version "main"
+  sha256 "a2415a02ecc0fea78a32f701c25cecfb313c06defd0a693e26a3afa57420721a" # Replace with actual sha256 if available
 
   def install
-    system "scripts/activate.sh"
-    system "./scripts/examples/gn_build_example.sh", "examples/chip-tool", "out/chip-tool"
-    bin.install "out/chip-tool/chip-tool"
+    bin.install "chip-tool"
   end
 
   test do
-    system "#{bin}/chip-tool", "pairing", "onnetwork", "1", "20202021" 
+    system "#{bin}/chip-tool", "pairing", "onnetwork", "1", "20202021"
   end
 end
