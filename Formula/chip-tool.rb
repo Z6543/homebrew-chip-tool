f192baa0035475729fce4b4c4c4675cb243413d8
cj-zoltan-balazs <60389717+ZoltanBalazs@users.noreply.github.com>
2025-08-23 12:38:31 +0200

    Update chip-tool formula URL and SHA256 for macOS binary

--- Formula/chip-tool.rb
+++ Formula/chip-tool.rb
@@ -2,9 +2,9 @@
 class ChipTool < Formula
   desc "Matter controller CLI tool from connectedhomeip"
   homepage "https://github.com/project-chip/connectedhomeip"
-  url "https://github.com/Z6543/homebrew-chip-tool/releases/download/v1.4.2.0/chip-tool"
+  url "https://github.com/Z6543/homebrew-chip-tool/releases/download/v1.4.2.0/chip-tool-macos-latest-arm64"
   version "v1.4.2.0"
-  sha256 "a2415a02ecc0fea78a32f701c25cecfb313c06defd0a693e26a3afa57420721a" # Replace with actual sha256 if available
+  sha256 "89aa65aa0ba78d488652c21d9ee7bb7ba2ee8669c2e9765bb7946d81892924c3" # Replace with actual sha256 if available
 
   def install
     bin.install "chip-tool"
