89cf34bbd8c550b5868c79c685f2d891ba4ad325
cj-zoltan-balazs <60389717+ZoltanBalazs@users.noreply.github.com>
2025-08-27 10:26:09 +0200

    Uncomment mv commands for binary renaming in install method

--- Formula/chip-tool.rb
+++ Formula/chip-tool.rb
@@ -18,10 +18,10 @@ class ChipTool < Formula
   def install
     if Hardware::CPU.intel?
       # system "unzip", "chip-tool-macOS-universal.bin.zip"
-      # mv "chip-tool-macOS-universal.bin", "chip-tool"
+      mv "chip-tool-macOS-universal.bin", "chip-tool"
       bin.install "chip-tool"
     else
-      # mv "chip-tool-macos-latest-arm64", "chip-tool"
+      mv "chip-tool-macos-latest-arm64", "chip-tool"
       bin.install "chip-tool"
     end
   end
